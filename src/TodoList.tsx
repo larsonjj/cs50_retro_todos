f3d4b6694f5b41de054f811dafdd371d313a10fb
Jake Larson <jake.j.larson@gmail.com>
2024-04-04 13:54:09 -0500

    Styled list items

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -8,7 +8,9 @@ interface TodoItem {
 }
 
 function App() {
-  const [todos, setTodos] = useState([] as TodoItem[]);
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", done: true },
+  ] as TodoItem[]);
 
   return (
     <div className="todoList">
@@ -21,6 +23,22 @@ function App() {
           />
           <button className="addButton">+</button>
         </div>
+        {todos.length > 0 && (
+          <div className="listWrapper">
+            <ul className="list">
+              {todos.map((todo) => (
+                <li
+                  key={todo.id}
+                  className={todo.done ? "listItem done" : "listItem"}
+                >
+                  <input type="checkbox" className="checkbox" />
+                  <div className="text">{todo.text}</div>
+                  <button className="deleteButton">x</button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
