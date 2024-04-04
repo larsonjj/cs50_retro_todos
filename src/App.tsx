d53b028dccd0976be1a716be92d14cfe0d277de5
Jake Larson <jake.j.larson@gmail.com>
2024-04-04 11:55:22 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Logo from "./Logo.tsx";
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Logo />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Logo.tsx
@@ -0,0 +1,8 @@
+import logoArt from "./assets/ascii/logo_art";
+import "./Logo.css";
+
+function Logo() {
+  return <pre>{logoArt}</pre>;
+}
+
+export default Logo;
