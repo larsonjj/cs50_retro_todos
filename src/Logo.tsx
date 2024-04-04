d046680273a261b25f88fe256909fb93d65aebc1
Jake Larson <jake.j.larson@gmail.com>
2024-04-04 11:59:01 -0500

    Made logo more mobile friendly

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -2,7 +2,7 @@ import logoArt from "./assets/ascii/logo_art";
 import "./Logo.css";
 
 function Logo() {
-  return <pre>{logoArt}</pre>;
+  return <pre className="logo">{logoArt}</pre>;
 }
 
 export default Logo;
