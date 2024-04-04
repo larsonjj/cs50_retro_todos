0d8921b927f33ca84b3dd0dbfa90ac877fc1fff9
Jake Larson <jake.j.larson@gmail.com>
2024-04-04 14:01:22 -0500

    Implemented logic to handle list items

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -8,21 +8,45 @@ interface TodoItem {
 }
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, text: "Learn React", done: true },
-  ] as TodoItem[]);
+  const [todos, setTodos] = useState([] as TodoItem[]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const input = e.currentTarget.querySelector("input") as HTMLInputElement;
+    const text = input.value.trim();
+    if (!text) return;
+    setTodos([...todos, { id: todos.length + 1, text, done: false }]);
+    input.value = "";
+  };
+
+  const handleCheckboxChange = (id: number) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, done: !todo.done };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const handleDeleteClick = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
   return (
     <div className="todoList">
       <div className="todoListInnerBorder">
-        <div className="inputWrapper">
-          <input
-            className="input"
-            type="text"
-            placeholder="What needs to be done?"
-          />
-          <button className="addButton">+</button>
-        </div>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="inputWrapper">
+            <input
+              className="input"
+              type="text"
+              placeholder="What needs to be done?"
+            />
+            <button className="addButton">+</button>
+          </div>
+        </form>
         {todos.length > 0 && (
           <div className="listWrapper">
             <ul className="list">
@@ -31,9 +55,19 @@ function App() {
                   key={todo.id}
                   className={todo.done ? "listItem done" : "listItem"}
                 >
-                  <input type="checkbox" className="checkbox" />
+                  <input
+                    type="checkbox"
+                    className="checkbox"
+                    checked={todo.done}
+                    onChange={() => handleCheckboxChange(todo.id)}
+                  />
                   <div className="text">{todo.text}</div>
-                  <button className="deleteButton">x</button>
+                  <button
+                    className="deleteButton"
+                    onClick={() => handleDeleteClick(todo.id)}
+                  >
+                    x
+                  </button>
                 </li>
               ))}
             </ul>
